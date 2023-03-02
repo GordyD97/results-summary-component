99b8c7bee8214cd1af72af65186514d84e7cdae7
Gordon D <gordon.draine97@yahoo.com>
2023-03-02 12:04:01 -0500

    adding tailwind and starting with a wire frame

--- src/App.jsx
+++ src/App.jsx
@@ -7,26 +7,27 @@ function App() {
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='mx-auto'>
+        {/* big container */}
+        <div className='leftcard'>
+            <h2>your result</h2>
+
+            <div className='score'>
+              <h1>76</h1>
+                <p>out of 100</p>
+
+                <h2> Great</h2>
+                <p> You score d higher than 65% of the poeple who have taken these test.</p>
+            </div>
+            
+        </div>
+        {/* this is the seperation between left card and right card. */}
+
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+
+
     </div>
   )
 }
