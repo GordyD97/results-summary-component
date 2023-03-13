8410e0aa0e63147c4a0f2c80126abcc1162f5aef
Gordon D <gordon.draine97@yahoo.com>
2023-03-12 22:36:23 -0400

    attempting to solve the data transfer problem for styling each score

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,14 @@ import data from "./data.json";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+
 
   return (
     <div className="App">
       {/* prob need to set the wole container for regulating two big collumns then the riht card will have a bunch of rows. */}
       <div className="mx-auto columns-2	gap-3">
         {/* big container */}
-        <div className="bg-black rounded-lg overflow-hidden shadow-md h-96">
+        <div className="bg-gray-400 rounded-lg overflow-hidden shadow-md h-96">
           <h2>your result</h2>
 
           <div className="right card">
@@ -34,30 +34,29 @@ function App() {
         </div>
         {/* this is the seperation between left card and right card. */}
 
-        <div className="bg-black rounded-lg overflow-hidden shadow-md h-96">
+        <div className="bg-gray-400 rounded-lg overflow-hidden shadow-md h-96">
           <h2>Sumary</h2>
-              <div className='summary-list'>
-                      {data.map((data) => 
-                          <Summary
-                              key={data.category}
-                              category={data.category}
-                              score={data.score}
-                              icon={data.icon}
-                              background={data.background}
-                      />
-                      )}
-          
+          <div className="summary-list">
+            {data.map((data) => (
+              <Summary
+                key={data.category}
+                category={data.category}
+                score={data.score}
+                icon={data.icon}
+                background={data.background}
+              />
+            ))}
 
-          <button
-            className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
-            type="submit"
-          >
-            {" "}
-            Continue
-          </button>
+            <button
+              className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded"
+              type="submit"
+            >
+              {" "}
+              Continue
+            </button>
+          </div>
         </div>
       </div>
-    </div>
     </div>
   );
 }
--- src/components/summary.jsx
+++ src/components/summary.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "../App.css";
+import data from "../data.json";
 
 export default function Summary(props) {
     return (
